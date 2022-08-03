d92c5ec6a1724ac11c1f400dc7a092b08c5498e2
hashitaku <hashitaku-dev@outlook.jp>
2022-08-03 19:12:49 +0900

    modified:   polybar/config.ini modified:   polybar/modules/backlight.ini modified:   polybar/modules/cpu.ini modified:   polybar/modules/date.ini modified:   polybar/modules/input_method.ini modified:   polybar/modules/network.ini modified:   polybar/modules/pulseaudio.ini
    new file:   polybar/scripts/pulseaudio-mic.sh
    new file:   polybar/modules/memory.ini
    new file:   polybar/modules/pulseaudio-mic.ini

---
+++ home/.config/polybar/scripts/pulseaudio-mic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+is_mute="$(pactl -f json list sources 2>/dev/null | jq -r --arg default "$(pactl get-default-source)" '.[] | select(.name == $default).mute')"
+
+if $is_mute; then
+    echo " muted"
+    #return 1
+else
+    echo -n " "
+    pactl -f json list sources 2>/dev/null | jq -r --arg default "$(pactl get-default-source)" '.[] | select(.name == $default).volume | map(.value_percent) | join(",")'
+fi
