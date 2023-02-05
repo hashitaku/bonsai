fa170bd0f786c42a6e402046d663e45dd3095be4
hashitaku <hashitaku-dev@outlook.jp>
2023-02-05 22:18:52 +0900

    modified:   home/.config/wezterm/wezterm.lua

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,27 +1,28 @@
-local wezterm = require "wezterm"
+local wezterm = require('wezterm')
 
 local target = wezterm.target_triple
 
 local window_decorations
 local shell
 local font_name
-if target == "x86_64-unknown-linux-gnu" then
-    window_decorations = "RESIZE"
-    shell = "bash"
-elseif target == "x86_64-pc-windows-msvc" then
-    window_decorations = "TITLE | RESIZE"
-    shell = "powershell"
+if target == 'x86_64-unknown-linux-gnu' then
+    window_decorations = 'RESIZE'
+    shell = 'bash'
+elseif target == 'x86_64-pc-windows-msvc' then
+    window_decorations = 'TITLE | RESIZE'
+    shell = 'powershell'
 end
 
 return {
     default_prog = {
         shell
     },
-    
+
     initial_cols = 120,
     initial_rows = 30,
 
     hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 0.95,
     window_decorations = window_decorations,
     window_padding = {
         left = 0,
@@ -30,46 +31,46 @@ return {
         bottom = 0,
     },
 
-    default_cursor_style = "BlinkingUnderline",
+    default_cursor_style = 'BlinkingUnderline',
     cursor_blink_rate = 500,
-    cursor_blink_ease_in = "Constant",
-    cursor_blink_ease_out = "Constant",
+    cursor_blink_ease_in = 'Constant',
+    cursor_blink_ease_out = 'Constant',
     animation_fps = 1,
 
     font = wezterm.font_with_fallback {
-        { family = "UbuntuMono Nerd Font Mono", weight = "Bold" },
-        { family = "Consolas", weight = "Bold" },
+        { family = 'UbuntuMono Nerd Font Mono', weight = 'Bold' },
+        { family = 'Consolas', weight = 'Bold' },
 
-        { family = "Noto Sans Mono CJK JP", weight = "Bold" },
+        { family = 'Noto Sans Mono CJK JP', weight = 'Bold' },
     },
     font_size = 14.0,
 
     colors = {
-        foreground = "#f8f8f2",
-        background = "#2c3643",
-        cursor_bg = "#ffffff",
-        selection_bg = "#222a34",
+        foreground = '#f8f8f2',
+        background = '#2c3643',
+        cursor_bg = '#ffffff',
+        selection_bg = '#222a34',
 
         ansi = {
-            "#666666",
-            "#fa5e5b",
-            "#16c98d",
-            "#ffc83f",
-            "#288ad6",
-            "#b267e6",
-            "#89bde4",
-            "#f8f8f2",
+            '#666666',
+            '#fa5e5b',
+            '#16c98d',
+            '#ffc83f',
+            '#288ad6',
+            '#b267e6',
+            '#89bde4',
+            '#f8f8f2',
         },
 
         brights = {
-            "#888888",
-            "#ff708e",
-            "#79c9af",
-            "#ffe299",
-            "#81b1d6",
-            "#bf8ae6",
-            "#b6cfe3",
-            "#f8f8f0",
+            '#888888',
+            '#ff708e',
+            '#79c9af',
+            '#ffe299',
+            '#81b1d6',
+            '#bf8ae6',
+            '#b6cfe3',
+            '#f8f8f0',
         },
     },
 }
