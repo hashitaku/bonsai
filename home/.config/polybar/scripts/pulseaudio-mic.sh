0188aba5bebf9cd4a86a3155a9ef2424dba67441
hashitaku <hashitaku-dev@outlook.jp>
2023-06-04 22:35:03 +0900

    fix: config/polybar
    polybarのオーディオモジュールのアイコンが表示されていなかったので修正

--- home/.config/polybar/scripts/pulseaudio-mic.sh
+++ home/.config/polybar/scripts/pulseaudio-mic.sh
@@ -3,9 +3,9 @@
 is_mute="$(pactl -f json list sources 2>/dev/null | jq -r --arg default "$(pactl get-default-source)" '.[] | select(.name == $default).mute')"
 
 if $is_mute; then
-    echo " muted"
+    echo " muted"
     #return 1
 else
-    echo -n " "
+    echo -n " "
     pactl -f json list sources 2>/dev/null | jq -r --arg default "$(pactl get-default-source)" '.[] | select(.name == $default).volume | map(.value_percent) | join(",")'
 fi
