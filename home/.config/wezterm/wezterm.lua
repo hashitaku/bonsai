34107ed376d230ae59d5047e1c5d4a7047e5ff1b
hashitaku <34015121+hashitaku@users.noreply.github.com>
2023-09-26 08:39:02 +0900

    fix: wezterm config
    styluaの設定が異なっていたため修正
    変数定義が足りなかったため追加

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -3,78 +3,79 @@ local wezterm = require("wezterm")
 local target = wezterm.target_triple
 
 local shell
+local hide_tab_bar_if_only_one_tab
 if target == "x86_64-unknown-linux-gnu" then
-	shell = {
-		"bash",
-	}
-	hide_tab_bar_if_only_one_tab = true
+    shell = {
+        "bash",
+    }
+    hide_tab_bar_if_only_one_tab = true
 elseif target == "x86_64-pc-windows-msvc" then
-	shell = {
-		"pwsh",
-		"-NoLogo",
-	}
-	hide_tab_bar_if_only_one_tab = false
+    shell = {
+        "pwsh",
+        "-NoLogo",
+    }
+    hide_tab_bar_if_only_one_tab = false
 end
 
 return {
-	webgpu_power_preference = "HighPerformance",
-	front_end = "WebGpu",
-	default_prog = shell,
+    webgpu_power_preference = "HighPerformance",
+    front_end = "WebGpu",
+    default_prog = shell,
 
-	initial_cols = 150,
-	initial_rows = 40,
+    initial_cols = 150,
+    initial_rows = 40,
 
-	hide_tab_bar_if_only_one_tab = hide_tab_bar_if_only_one_tab,
-	window_background_opacity = 0.95,
-	window_decorations = "RESIZE",
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+    hide_tab_bar_if_only_one_tab = hide_tab_bar_if_only_one_tab,
+    window_background_opacity = 0.95,
+    window_decorations = "RESIZE",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 
-	default_cursor_style = "BlinkingUnderline",
-	cursor_blink_rate = 500,
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-	animation_fps = 1,
+    default_cursor_style = "BlinkingUnderline",
+    cursor_blink_rate = 500,
+    cursor_blink_ease_in = "Constant",
+    cursor_blink_ease_out = "Constant",
+    animation_fps = 1,
 
-	font = wezterm.font_with_fallback({
-		{ family = "UbuntuMono Nerd Font", weight = "Bold" },
-		{ family = "Consolas", weight = "Bold" },
+    font = wezterm.font_with_fallback({
+        { family = "UbuntuMono Nerd Font", weight = "Bold" },
+        { family = "Consolas", weight = "Bold" },
 
-		{ family = "Noto Sans Mono CJK JP", weight = "Bold" },
-	}),
-	font_size = 14.0,
-	bold_brightens_ansi_colors = true,
+        { family = "Noto Sans Mono CJK JP", weight = "Bold" },
+    }),
+    font_size = 16.0,
+    bold_brightens_ansi_colors = true,
 
-	colors = {
-		foreground = "#f8f8f2",
-		background = "#2c3643",
-		cursor_bg = "#ffffff",
-		selection_bg = "#222a34",
+    colors = {
+        foreground = "#f8f8f2",
+        background = "#2c3643",
+        cursor_bg = "#ffffff",
+        selection_bg = "#222a34",
 
-		ansi = {
-			"#666666",
-			"#fa5e5b",
-			"#16c98d",
-			"#ffc83f",
-			"#288ad6",
-			"#b267e6",
-			"#89bde4",
-			"#f8f8f2",
-		},
+        ansi = {
+            "#666666",
+            "#fa5e5b",
+            "#16c98d",
+            "#ffc83f",
+            "#288ad6",
+            "#b267e6",
+            "#89bde4",
+            "#f8f8f2",
+        },
 
-		brights = {
-			"#888888",
-			"#ff708e",
-			"#79c9af",
-			"#ffe299",
-			"#81b1d6",
-			"#bf8ae6",
-			"#b6cfe3",
-			"#f8f8f0",
-		},
-	},
+        brights = {
+            "#888888",
+            "#ff708e",
+            "#79c9af",
+            "#ffe299",
+            "#81b1d6",
+            "#bf8ae6",
+            "#b6cfe3",
+            "#f8f8f0",
+        },
+    },
 }
