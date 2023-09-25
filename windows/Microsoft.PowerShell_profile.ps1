412f9a68798d49803f363c5b976863bfb71acd14
hashitaku <34015121+hashitaku@users.noreply.github.com>
2023-09-26 06:40:51 +0900

    fix: powershell config file
    エンコーディングの設定を変更。
    viモードの設定の追加。
    必要なコマンドのセットアップを追加。

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,31 @@
-chcp 65001 | Out-Null
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
+$OnViModeChange = {
+    if ($args[0] -eq "Command") {
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
 
 Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
+
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
-Set-PSReadLineKeyHandler -Chord 'Ctrl+[' -Function ViCommandMode
+# [Console]::ReadKey()
+# https://github.com/PowerShell/PSReadLine/issues/906
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Oem4' -ViMode Insert -Function ViCommandMode
 
-function Prompt {
-    Write-Host "$(Get-Location)" `
-    -NoNewLine `
-    -ForegroundColor Green
+if (Get-Command -ErrorAction SilentlyContinue fnm) {
+    fnm env --use-on-cd | Out-String | Invoke-Expression
+    fnm completions --shell power-shell | Out-String | Invoke-Expression
+}
 
-    Write-Host ">" `
-    -NoNewLine
+if (Get-Command -ErrorAction SilentlyContinue rustup) {
+    rustup completions powershell rustup | Out-String | Invoke-Expression
+}
 
-    return " "
+if (Get-Module -ListAvailable -Name posh-git) {
+    Import-Module posh-git
 }
