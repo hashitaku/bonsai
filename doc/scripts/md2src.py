e2b7f5ccd55fa1b73d1108ab49a018aa3f7867fc
hashitaku <34015121+hashitaku@users.noreply.github.com>
2023-12-06 00:01:52 +0900

    fix: エンコーディングを明記
    windowsなどのutf-8がデフォルトエンコーディングではない環境でエラーが出るため修正

--- doc/scripts/md2src.py
+++ doc/scripts/md2src.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.exit("usage: md2src [input.md] [output.*]")
 
-    with open(sys.argv[1]) as infile, open(sys.argv[2], "w") as outfile:
+    with open(sys.argv[1], "r", encoding="utf-8") as infile, open(sys.argv[2], "w", encoding="utf-8") as outfile:
         while line := infile.readline():
             if "```" in line:
                 indent = line.count(" ", 0, line.find("```"))
