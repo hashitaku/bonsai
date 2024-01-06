897250744d9c5df6e0943eabc270c1f2c55b83dd
hashitaku <34015121+hashitaku@users.noreply.github.com>
2024-01-07 04:23:33 +0900

    update: PowerShell設定ファイルの更新
    文字エンコードをUTF-8の設定
    PSReadLineの予測文字列の色の設定
    oh-my-poshの設定
    基本コマンドをuutilsへ置き換え

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::InputEncoding = [System.Text.Encoding]::UTF8
+$PSDefaultParameterValues["*:Encoding"] = "utf8"
 
 $OnViModeChange = {
     if ($args[0] -eq "Command") {
@@ -11,6 +13,8 @@ $OnViModeChange = {
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
+
+Set-PSReadLineOption -Colors @{ "InlinePrediction"="`e[38;2;153;169;179m" }
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 # [Console]::ReadKey()
@@ -26,6 +30,25 @@ if (Get-Command -ErrorAction SilentlyContinue rustup) {
     rustup completions powershell rustup | Out-String | Invoke-Expression
 }
 
-if (Get-Module -ListAvailable -Name posh-git) {
-    Import-Module posh-git
+if (Get-Command -ErrorAction SilentlyContinue oh-my-posh) {
+    oh-my-posh --config "${ENV:HOMEDRIVE}${ENV:HOMEPATH}\.config\oh-my-posh\config.toml" init pwsh | Invoke-Expression
+}
+
+if (Get-Command -ErrorAction SilentlyContinue uutils) {
+    Remove-Alias -Name @(
+        "cat"
+        "cp"
+        "echo"
+        "ls"
+        "mv"
+        "pwd"
+        "rm"
+        "rmdir"
+    )
+
+    function ls_impl { ls.exe --color=auto $Args }
+    Set-Alias -Name "ls" -Value "ls_impl"
+
+    # mkdirはPowershellでは組み込みの関数なのでエイリアスで上書き
+    Set-Alias -Name "mkdir" -Value "mkdir.exe"
 }
