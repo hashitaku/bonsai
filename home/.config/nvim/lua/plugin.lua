6ef7e94d84c4b3509299b8ca93de022958062cab
Takuma Hashimoto <34015121+hashitaku@users.noreply.github.com>
2024-03-19 11:33:20 +0900

    update: neovim config
    treesitterにyamlを追加

--- home/.config/nvim/lua/plugin.lua
+++ home/.config/nvim/lua/plugin.lua
@@ -508,6 +508,7 @@ return {
                     "typescript",
                     "vim",
                     "vimdoc",
+                    "yaml",
                 },
                 sync_intall = true,
                 highlight = {
