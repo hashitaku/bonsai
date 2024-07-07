4bf8bb45661bb7c84f18d153ff4bc961ca704c7d
hashitaku <hashitaku-dev@outlook.jp>
2024-07-08 08:45:57 +0900

    add: neovim config ex command
    カーソルしたのsnake_caseとcamelCaseを相互変換するコマンドの追加

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -93,6 +93,28 @@ vim.api.nvim_create_autocmd("TermClose", {
     end,
 })
 
+-- cmd
+vim.api.nvim_create_user_command("ToSnakeFromCamel", function(_)
+    local cword = vim.fn.expand("<cword>")
+
+    local snake = cword:gsub("(%u)", "_%1"):gsub("^_", ""):lower()
+
+    vim.cmd.normal({ args = { "ciw" .. snake }, bang = true })
+    vim.cmd.normal("b")
+end, {})
+vim.api.nvim_create_user_command("ToCamelFromSnake", function(_)
+    local cword = vim.fn.expand("<cword>")
+
+    local camel = cword
+        :gsub("_(%l)", function(c)
+            return c:upper()
+        end)
+        :gsub("^_", "")
+
+    vim.cmd.normal({ args = { "ciw" .. camel }, bang = true })
+    vim.cmd.normal("b")
+end, {})
+
 vim.cmd.packadd("termdebug")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
