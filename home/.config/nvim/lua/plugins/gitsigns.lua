ddc8ac9eb61148470fa6c53c39b3d3b7ecd5de24
hashitaku <hashitaku-dev@outlook.jp>
2024-07-27 17:56:46 +0900

    update: neovim config
    gitsignsを追加

---
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        enabled = not vim.g.vscode,
+        opts = {
+            preview_config = {
+                border = "rounded",
+            },
+        },
+    },
+}
