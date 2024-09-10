5fd7a6ec3844a5d69a6009060e2c6313f1b2bd01
hashitaku <hashitaku-dev@outlook.jp>
2024-09-10 11:20:53 +0900

    update: neovim config
    カラーコードから色を表示するプラグイン'nvim-highlight-colors'の追加

---
+++ home/.config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "brenoprata10/nvim-highlight-colors",
+        enabled = not vim.g.vscode,
+        config = function()
+            require("nvim-highlight-colors").setup({
+                render = "virtual",
+                virtual_symbol = "󱓻",
+                virtual_symbol_prefix = "",
+                virtual_symbol_suffix = "",
+                virtual_symbol_position = "eol",
+            })
+
+            vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+                callback = function()
+                    require("nvim-highlight-colors").turnOff()
+                end,
+            })
+
+            vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+                callback = function()
+                    require("nvim-highlight-colors").turnOn()
+                end,
+            })
+        end,
+    },
+}
