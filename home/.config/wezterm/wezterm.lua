e88369ff8ce250bf99d9da62e7b72052d4b8ca7d
hashitaku <hashitaku-dev@outlook.jp>
2024-09-11 15:18:00 +0900

    update: wezterm config
    デフォルトのカーソルをSteadyBarにした

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -51,7 +51,7 @@ return {
     },
     enable_kitty_keyboard = true,
 
-    default_cursor_style = "BlinkingBar",
+    default_cursor_style = "SteadyBar",
     cursor_blink_rate = 500,
     cursor_blink_ease_in = "Constant",
     cursor_blink_ease_out = "Constant",
