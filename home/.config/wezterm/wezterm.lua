7b238c769c3939b9aa54d1be15f6754ae7c5574e
hashitaku <hashitaku-dev@outlook.jp>
2024-10-02 13:02:32 +0900

    update: wezterm config
    - `config_builder`を使用するように変更
    - ウィンドウサイズが変わったときにフォントサイズを変更することでウィンドウ下部に隙間ができにくいようにした
    - タブバーの背景をなしにした

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -17,80 +17,127 @@ elseif target == "x86_64-pc-windows-msvc" then
     hide_tab_bar_if_only_one_tab = false
 end
 
-return {
-    webgpu_power_preference = "HighPerformance",
-    front_end = "WebGpu",
-    default_prog = shell,
+local config = wezterm.config_builder()
 
-    initial_cols = 130,
-    initial_rows = 30,
+config.webgpu_power_preference = "HighPerformance"
+config.front_end = "WebGpu"
+config.default_prog = shell
 
-    hide_tab_bar_if_only_one_tab = hide_tab_bar_if_only_one_tab,
-    window_background_opacity = 1.0,
-    window_decorations = "RESIZE",
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-    },
-    window_frame = {
-        font = wezterm.font({
-            family = "Noto Sans Mono CJK JP",
-            weight = "Bold"
-        }),
-        font_size = 8,
-    },
+config.initial_cols = 130
+config.initial_rows = 30
 
-    keys = {
-        {
-            key = "K",
-            mods = "SHIFT|CTRL",
-            action = wezterm.action.DisableDefaultAssignment,
-        },
-    },
-    enable_kitty_keyboard = true,
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = hide_tab_bar_if_only_one_tab
+config.window_background_opacity = 1
+config.window_decorations = "RESIZE"
 
-    default_cursor_style = "SteadyBar",
-    cursor_blink_rate = 500,
-    cursor_blink_ease_in = "Constant",
-    cursor_blink_ease_out = "Constant",
-    animation_fps = 1,
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
 
+config.window_frame = {
     font = wezterm.font_with_fallback({
-        { family = "Inconsolata Nerd Font Mono", weight = "Bold" },
+        { family = "Inconsolata Nerd Font Mono", weight = "Regular" },
 
-        { family = "Noto Sans Mono CJK JP", weight = "Bold" },
+        { family = "Noto Sans Mono CJK JP",      weight = "Regular" },
     }),
-    font_size = 14,
-    bold_brightens_ansi_colors = true,
+    font_size = 10,
+    active_titlebar_bg = "none",
+    inactive_titlebar_bg = "none",
+}
 
-    colors = {
-        foreground = "#f8f8f2",
-        background = "#2c3643",
-        cursor_bg = "#ffffff",
-        selection_bg = "#222a34",
-
-        ansi = {
-            "#666666",
-            "#fa5e5b",
-            "#16c98d",
-            "#ffc83f",
-            "#288ad6",
-            "#b267e6",
-            "#89bde4",
-            "#f8f8f2",
-        },
-
-        brights = {
-            "#888888",
-            "#ff708e",
-            "#79c9af",
-            "#ffe299",
-            "#81b1d6",
-            "#bf8ae6",
-            "#b6cfe3",
-            "#f8f8f0",
-        },
+config.keys = {
+    {
+        key = "K",
+        mods = "SHIFT|CTRL",
+        action = wezterm.action.DisableDefaultAssignment,
     },
 }
+config.enable_kitty_keyboard = true
+
+config.default_cursor_style = "SteadyBar"
+config.cursor_blink_rate = 500
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.animation_fps = 1
+
+config.font = wezterm.font_with_fallback({
+    { family = "Inconsolata Nerd Font Mono", weight = "Bold" },
+
+    { family = "Noto Sans Mono CJK JP",      weight = "Bold" },
+})
+config.font_size = 14
+config.bold_brightens_ansi_colors = true
+
+config.colors = {
+    foreground = "#f8f8f2",
+    background = "#2c3643",
+    cursor_bg = "#f8f8f2",
+    selection_bg = "#222a34",
+
+    ansi = {
+        "#666666",
+        "#fa5e5b",
+        "#16c98d",
+        "#ffc83f",
+        "#288ad6",
+        "#b267e6",
+        "#89bde4",
+        "#f8f8f2",
+    },
+
+    brights = {
+        "#888888",
+        "#ff708e",
+        "#79c9af",
+        "#ffe299",
+        "#81b1d6",
+        "#bf8ae6",
+        "#b6cfe3",
+        "#f8f8f0",
+    },
+
+    tab_bar = {
+        inactive_tab_edge = "none",
+    },
+}
+
+local function change_font_size(window, pane)
+    local font_range = {
+        min = 14.0,
+        max = 16.0,
+    }
+    local font_size_delta = 0.5
+
+    local best_font_size = font_range.min
+    local min_diff = window:get_dimensions().pixel_height - pane:get_dimensions().pixel_height
+    for font_size = font_range.min, font_range.max, font_size_delta do
+        local overrides = window:get_config_overrides() or {}
+
+        overrides.font_size = font_size
+
+        window:set_config_overrides(overrides)
+
+        local current_diff = window:get_dimensions().pixel_height - pane:get_dimensions().pixel_height
+
+        if current_diff < min_diff then
+            min_diff = current_diff
+            best_font_size = font_size
+        end
+    end
+
+    local overrides = window:get_config_overrides() or {}
+
+    overrides.font_size = best_font_size
+
+    window:set_config_overrides(overrides)
+end
+
+wezterm.on("window-resized", function(window, pane)
+    change_font_size(window, pane)
+end)
+
+return config
