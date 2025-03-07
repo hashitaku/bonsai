883f06d303552ae93828bc0cad554d9013450b53
hashitaku <hashitaku-dev@outlook.jp>
2025-03-07 11:19:28 +0900

    update: neovim config
    ':grep'のメッセージを表示しない、初めにマッチした箇所へ移動しないコマンド'Grep'を追加

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -102,6 +102,11 @@ vim.api.nvim_create_user_command("ToCamelFromSnake", function(_)
     vim.cmd.normal({ args = { "ciw" .. camel }, bang = true })
     vim.cmd.normal("b")
 end, {})
+vim.api.nvim_create_user_command("Grep", function(tbl)
+    vim.cmd.grep({ args = { tbl.args }, bang = true, mods = { silent = true } })
+end, {
+    nargs = 1,
+})
 
 -- other
 vim.diagnostic.config({
