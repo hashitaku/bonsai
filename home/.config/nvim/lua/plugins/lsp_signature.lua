8fc314efe493e6139d9661d5c7e91718de3ca0d0
hashitaku <hashitaku-dev@outlook.jp>
2025-03-07 10:36:07 +0900

    update: neovim config
    ray-x/lsp_signature.nvimの設定を変更
    
    - signature_helpのfloatingwindowのサイズが改行、省略されないサイズにした
    - virtual textにヒントを表示しないようにした

--- home/.config/nvim/lua/plugins/lsp_signature.lua
+++ home/.config/nvim/lua/plugins/lsp_signature.lua
@@ -2,6 +2,17 @@ return {
     {
         "ray-x/lsp_signature.nvim",
         enabled = not vim.g.vscode,
-        opts = {},
+        opts = {
+            -- どれぐらいのsignature_helpの大きさになると困るのか不明なため大きな値にする
+            max_width = 1000,
+            max_height = 1000,
+            wrap = false,
+            hint_enable = false,
+            hint_prefix = {
+                above = "󱞢 ", -- f17a2
+                current = "󰜱 ", -- f0731
+                below = "󱞾 ", -- f17be
+            },
+        },
     },
 }
