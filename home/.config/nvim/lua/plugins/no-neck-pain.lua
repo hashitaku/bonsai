29d7ed091cb0fe6f6dfe7343b66c65faecb93dee
hashitaku <hashitaku-dev@outlook.jp>
2025-03-11 09:16:41 +0900

    update: neovim config
    no-neck-painの設定を変更

--- home/.config/nvim/lua/plugins/no-neck-pain.lua
+++ home/.config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,5 +1,8 @@
 return {
     "shortcuts/no-neck-pain.nvim",
     enabled = not vim.g.vscode,
-    opts = {},
+    opts = {
+        -- コードが表示されている幅が100であってほしいため少し大きい数を指定
+        width = 105,
+    },
 }
