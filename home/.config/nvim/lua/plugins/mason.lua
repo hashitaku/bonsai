084197fe886be9b4fb7d24e94f6a5498e8722407
hashitaku <hashitaku-dev@outlook.jp>
2025-04-12 15:50:56 +0900

    update: neovim config
    mason-lspconfigの依存関係を追加

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -20,9 +20,9 @@ return {
 
     {
         "williamboman/mason-lspconfig.nvim",
-        -- dependencies = {
-        --     "williamboman/mason.nvim",
-        -- },
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
         opts = {
             ensure_installed = {
                 "ts_ls",
