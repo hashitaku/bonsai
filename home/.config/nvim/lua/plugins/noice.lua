057c2578a44136e0a92cd2fc6d26be890d241cb8
hashitaku <hashitaku-dev@outlook.jp>
2025-06-11 13:47:16 +0900

    update: neovim config
    NoiceAllなどのコマンドで表示されるウィンドウにスクロールバーが表示されないようにした

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -7,9 +7,31 @@ return {
                 enabled = true,
                 view = "notify",
             },
+            commands = {
+                history = {
+                    opts = {
+                        scrollbar = false,
+                    },
+                },
+                last = {
+                    opts = {
+                        scrollbar = false,
+                    },
+                },
+                errors = {
+                    opts = {
+                        scrollbar = false,
+                    },
+                },
+                all = {
+                    opts = {
+                        scrollbar = false,
+                    },
+                },
+            },
             presets = {
                 command_palette = false,
-                lsp_doc_border = true,
+                lsp_doc_border = false,
             },
             lsp = {
                 hover = {
