30639e84991dcb659c72c7027f520ccde5a7aaa9
hashitaku <hashitaku-dev@outlook.jp>
2025-10-22 15:56:18 +0900

    update: neovim config
    powershellスクリプトのときのみバッククォートの補完を行わないようにした

--- home/.config/nvim/lua/plugins/autopairs.lua
+++ home/.config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,14 @@ return {
     {
         "windwp/nvim-autopairs",
         cond = not vim.g.vscode,
-        opts = {},
+        config = function()
+            local npairs = require("nvim-autopairs")
+
+            npairs.setup({
+                check_ts = true,
+            })
+
+            npairs.get_rules("`")[1].not_filetypes = { "ps1" }
+        end,
     },
 }
