46c28c725584c58274ccb4092cfd57594aa3132b
hashitaku <hashitaku-dev@outlook.jp>
2026-02-12 14:14:18 +0900

    update: neovim config
    treesitterの最新版に追従した

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,44 +2,52 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         cond = not vim.g.vscode,
+        branch = "main",
         config = function()
-            vim.api.nvim_set_option_value("foldlevelstart", 1, {})
-            vim.api.nvim_set_option_value("foldmethod", "expr", {})
-            vim.api.nvim_set_option_value("foldexpr", "v:lua.vim.treesitter.foldexpr()", {})
-            vim.api.nvim_set_option_value("foldtext", "", {})
+            local parser = {
+                "bash",
+                "c",
+                "c_sharp",
+                "cmake",
+                "cpp",
+                "css",
+                "go",
+                "html",
+                "json",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "meson",
+                "powershell",
+                "python",
+                "regex",
+                "rust",
+                "slint",
+                "toml",
+                "typescript",
+                "typst",
+                "vim",
+                "vimdoc",
+                "yaml",
+            }
 
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "c_sharp",
-                    "cmake",
-                    "cpp",
-                    "css",
-                    "go",
-                    "html",
-                    "json",
-                    "lua",
-                    "make",
-                    "markdown",
-                    "markdown_inline",
-                    "meson",
-                    "powershell",
-                    "python",
-                    "regex",
-                    "rust",
-                    "slint",
-                    "toml",
-                    "typescript",
-                    "typst",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-                sync_intall = true,
-                highlight = {
-                    enable = true,
-                },
+            require("nvim-treesitter").install(parser)
+
+            local user_treesitter_augid = vim.api.nvim_create_augroup("user_treesitter", { clear = false })
+            vim.api.nvim_create_autocmd({ "FileType" }, {
+                group = user_treesitter_augid,
+                callback = function(ev)
+                    if vim.list_contains(parser, ev.match) then
+                        local success, result = pcall(vim.treesitter.start)
+
+                        vim.api.nvim_set_option_value("foldlevelstart", 1, {})
+                        vim.api.nvim_set_option_value("foldmethod", "expr", {})
+                        vim.api.nvim_set_option_value("foldexpr", "v:lua.vim.treesitter.foldexpr()", {})
+                        vim.api.nvim_set_option_value("foldtext", "", {})
+                    end
+
+                end,
             })
         end,
     },
